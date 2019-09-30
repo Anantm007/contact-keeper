6fdc024fe186be7cb0b7c02667e34d6da5eb5142
Anant <anant.mathur007@gmail.com>
2019-09-30 15:53:42 +0530

    React cleanup and navbar

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>About this App</h1>
+
+            <p className="my-1">
+                This is a full stack MERN app for keeping contacts
+            </p>
+
+            <p className="bg-dark p">
+                <string>Version: </string> 1.0.0
+            </p>
+        </div>
+    )
+}
+
+export default Home
