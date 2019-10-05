144e3ee53206dabca1b2cb4b83e7e2fcbf5c3e52
Anant Mathur <anant.mathur007@gmail.com>
2019-10-05 17:18:20 +0530

    Contact from and delete

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Contacts from '../contacts/Contacts';
+import ContactForm from '../contacts/ContactForm';
 
 const Home = () => {
     return (
         <div className="grid-2">
             <div>
-                {/*Contact form*/}
-                <div>
+                <ContactForm/>
+            </div>
+            <div>
                     <Contacts/>
-                </div>
+                
             </div>
         </div>
     )
