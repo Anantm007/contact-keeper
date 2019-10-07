92d8c98fbbac44620d32890f4e8a7e6f3d8fc7e4
Anant Mathur <anant.mathur007@gmail.com>
2019-10-07 16:48:29 +0530

    Update About Page

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -6,12 +6,20 @@ const Home = () => {
             <h1>About this App</h1>
 
             <p className="my-1">
-                This is a full stack MERN app for keeping contacts
+                This is a full stack MERN app for keeping contact details such as email and phone numbers.
             </p>
 
-            <p className="bg-dark p">
-                <string>Version: </string> 1.0.0
-            </p>
+            <h3>
+                Made with ❤ by Anant Mathur
+            </h3>
+
+            <h4 ><a href="https://github.com/Anantm007/contact-keeper" target="_blank">
+                Github repo</a>
+            </h4>
+            
+            <h4><a href="https://www.linkedin.com/in/anant-mathur-61911423/" target="_blank">
+                LinkedIn</a> 
+            </h4>
         </div>
     )
 }
